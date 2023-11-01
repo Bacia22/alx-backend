b4c64daaff855f044239d7bdf387ec230d1d8a41
Bacia22 <adolfobacia@yahoo.com>
2023-11-01 02:07:52 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-""" a basic flask app"""
+"""A simple flask app
+"""
+
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    """ render a basic html file """
+def hello_world():
+    """_summary_
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port="5000", host="0.0.0.0", debug=True)
