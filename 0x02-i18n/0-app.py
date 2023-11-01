63abedc5380ea4f4abacee7abd164576fbcbd6f5
Bacia22 <adolfobacia@yahoo.com>
2023-11-01 02:15:17 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-""" Flask application module """
+"""
+Basic Flask app
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def home():
-    """ Home route """
-    return render_template("0-index.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
+    return render_template('0-index.html')
